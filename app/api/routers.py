0dd44e781637b966331a7ef591b1ea5225929a43
moon_home <ans92175@gmail.com>
2025-12-04 22:43:23 +0900

    git test용 파일 삭제

--- app/api/routers.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.services.metrics_service import MetricsService
-
-router = APIRouter()
-
-@router.get("/metrics")
-def get_metrics():
-    return MetricsService.get_metrics()
